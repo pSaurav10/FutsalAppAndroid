0600bdc093e80483b6b7c879ecdccbcd57590ee9
pSaurav10 <sauravinmail@gmail.com>
2021-02-19 09:47:28 +0545

    User API login check

--- app/src/main/java/com/example/futsalapp/api/UserAPI.kt
+++ app/src/main/java/com/example/futsalapp/api/UserAPI.kt
@@ -4,6 +4,7 @@ import com.example.futsalapp.model.User
 import com.example.futsalapp.response.LoginSignupResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
@@ -12,6 +13,14 @@ interface UserAPI {
     @POST("/player/register")
     suspend fun registerPlayer(
         @Body user: User
+    ): Response<LoginSignupResponse>
+
+    //Login user
+    @FormUrlEncoded
+    @POST("auth/login")
+    suspend fun checkUser(
+            @Field("username") username : String,
+            @Field("password") password : String
     ): Response<LoginSignupResponse>
 
 }
