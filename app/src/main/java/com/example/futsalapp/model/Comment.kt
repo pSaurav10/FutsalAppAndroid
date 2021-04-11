d145fa2e6c2d7db00150576ec2fe5ec7e78088f6
pSaurav10 <sauravinmail@gmail.com>
2021-04-11 14:42:30 +0545

    Post model with comments

---
+++ app/src/main/java/com/example/futsalapp/model/Comment.kt
@@ -0,0 +1,15 @@
+package com.example.futsalapp.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Comment (
+        @PrimaryKey
+        var _id: String = "",
+        var comment: String? = null,
+        var cusername: String? = null,
+        var cuserimage: String? = null,
+        var ccreatedAt: String? = null,
+        var cuserid: String? = null
+)
