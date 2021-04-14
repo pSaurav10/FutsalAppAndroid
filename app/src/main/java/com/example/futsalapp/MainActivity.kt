c27c7c6b496bb242c35ccc6ae6835438c3e79083
pSaurav10 <sauravinmail@gmail.com>
2021-04-14 22:55:56 +0545

    Main User Page Fragment

--- app/src/main/java/com/example/futsalapp/MainActivity.kt
+++ app/src/main/java/com/example/futsalapp/MainActivity.kt
@@ -4,21 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.example.futsalapp.api.ServiceBuilder
-import com.example.futsalapp.db.FutsalDB
 import com.example.futsalapp.fragments.EventFragment
 import com.example.futsalapp.fragments.FutsalFragment
 import com.example.futsalapp.fragments.PostFragment
+import com.example.futsalapp.fragments.UserFragment
 import com.example.futsalapp.model.Futsal
 import com.example.futsalapp.repository.EventRepository
 import com.example.futsalapp.repository.FutsalRepository
 import com.example.futsalapp.repository.UserRepository
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Dispatchers.Main
@@ -29,13 +29,12 @@ class MainActivity : AppCompatActivity() {
 
 
     private lateinit var bottom_layout: BottomNavigationView
-    private lateinit var user_img: CircleImageView
+    private lateinit var user_img: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         user_img = findViewById(R.id.user_img)
         bottom_layout = findViewById(R.id.bottom_layout)
-        retrievePlayer()
         menuitem()
         retrieveFutsal()
         retrieveEvent()
@@ -50,35 +49,7 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-    private fun retrievePlayer() {
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                val userrepo = UserRepository()
-                val response = userrepo.getUser()
-                val success = response.success
-                if (success == true){
-                    val userdata = response.data!!
-                    val imagepath = ServiceBuilder.loadImagePath() + userdata.imagepp
-                    withContext(Main){
-                        Glide.with(this@MainActivity)
-                                .load(imagepath)
-                                .fitCenter()
-                                .into(user_img)
-                    }
-                }
-            }
-            catch (e: Exception){
-                withContext(Main) {
-                    Toast.makeText(
-                            this@MainActivity,
-                            "Error:::: ${e.toString()}", Toast.LENGTH_SHORT
-                    ).show()
-                    println("Error:::: ${e.toString()}")
-                }
-            }
 
-        }
-    }
 
     private fun menuitem() {
         bottom_layout.setOnNavigationItemSelectedListener { item ->
@@ -100,6 +71,13 @@ class MainActivity : AppCompatActivity() {
                 R.id.eventfrag -> {
                     supportFragmentManager.beginTransaction().apply{
                         replace(R.id.frame, EventFragment())
+                        commit()
+                    }
+                    true
+                }
+                R.id.userfrag -> {
+                    supportFragmentManager.beginTransaction().apply{
+                        replace(R.id.frame, UserFragment())
                         commit()
                     }
                     true
